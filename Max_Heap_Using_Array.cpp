4b13d2dbef99157894349cd6ac00a6181ef7013b
Ahmed <ahmedanwer222@gmail.com>
2021-07-07 04:03:14 +0200

    Add files via upload

---
+++ Max_Heap_Using_Array.cpp
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Heap {
+	int* arr;
+	int count;
+	int size;
+};
+
+Heap* initHeap(int size) {
+	Heap* heap = (Heap*)malloc(sizeof(Heap));
+	heap->arr = (int*)malloc(size * sizeof(int));
+	heap->count = 0;
+	heap->size = size;
+	return heap;
+}
+
+void shiftUp(Heap* heap, int index) {
+	int temp;
+	int parentIndex = (index - 1) / 2;
+	if (heap->arr[index] > heap->arr[parentIndex]) {
+		temp = heap->arr[index];
+		heap->arr[index] = heap->arr[parentIndex];
+		heap->arr[parentIndex] = temp;
+		shiftUp(heap, parentIndex);
+	}
+}
+
+void shiftDown(Heap* heap, int parentIndex) {
+	int temp;
+	int maxIndex;
+	int leftChildIndex = parentIndex * 2 + 1;
+	int rightChildIndex = parentIndex * 2 + 2;
+	if (leftChildIndex >= heap->count) leftChildIndex = -1;
+	if (rightChildIndex >= heap->count) rightChildIndex = -1;
+	if (leftChildIndex != -1 && heap->arr[leftChildIndex] > heap->arr[parentIndex]) {
+		maxIndex = leftChildIndex;
+	}
+	else {
+		maxIndex = parentIndex;
+	}
+	if (rightChildIndex != -1 && heap->arr[rightChildIndex] > heap->arr[maxIndex]) {
+		maxIndex = rightChildIndex;
+	}
+	if (maxIndex != parentIndex) {
+		temp = heap->arr[parentIndex];
+		heap->arr[parentIndex] = heap->arr[maxIndex];
+		heap->arr[maxIndex] = temp;
+		shiftDown(heap, maxIndex);
+	}
+}
+
+void insert(Heap* heap, int data) {
+	if (heap->count < heap->size) {
+		heap->arr[heap->count] = data;
+		shiftUp(heap, heap->count);
+		heap->count += 1;
+	}
+	else {
+		printf("Heap is Full\n");
+	}
+}
+
+int popMax(Heap* heap) {
+	int data;
+	if (heap->count != 0) {
+		data = heap->arr[0];
+		heap->arr[0] = heap->arr[heap->count - 1];
+		heap->count -= 1;
+		shiftDown(heap, 0);
+	}
+	else {
+		data = INT_MIN;
+	}
+	return data;
+}
+
+void printHeap(Heap* h) {
+	for (int i = 0; i < h->count; i++) {
+		printf(" %d ", h->arr[i]);
+	}
+	printf("\n");
+}
+
+void main() {
+	Heap* heap = initHeap(100);
+	insert(heap, 5);
+	insert(heap, 7);
+	insert(heap, 6);
+	insert(heap, 8);
+	insert(heap, 9);
+	insert(heap, 10);
+	insert(heap, 3);
+	popMax(heap);
+	insert(heap, 11);
+	printHeap(heap);
+}
